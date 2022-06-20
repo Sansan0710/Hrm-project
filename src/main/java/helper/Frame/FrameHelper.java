dfb52f8071733f44975da1623718966954af3e11
Lương Thị Thu Hà (ITBU KT) <haltt32@viettelimex.vn>
2022-06-20 18:40:46 +0700

    new commit

---
+++ src/main/java/helper/Frame/FrameHelper.java
@@ -0,0 +1,26 @@
+package helper.switchHelper;
+
+import helper.logger.LoggerHelper;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FrameHelper {
+    private WebDriver driver;
+    private Logger log = LoggerHelper.getLogger(FrameHelper.class);
+
+    public void switchToFrame(int index){
+        driver.switchTo().frame(index);
+        log.info("switched to: " + index + " frame");
+    }
+
+    public void switchToFrame(String frameName){
+        driver.switchTo().frame(frameName);
+        log.info("switched to: " + frameName + " frame");
+    }
+
+    public void switchToFrame(WebElement element){
+        driver.switchTo().frame(element);
+        log.info("switch to frame: " + element.toString());
+    }
+}
---
+++ src/main/java/helper/wait/WaitHelper.java
@@ -0,0 +1,2 @@
+package helper.wait;public class waitHelper {
+}
